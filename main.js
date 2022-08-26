a8487b7dd904c693f959780ba520d4ed5d944a50
BrendaCosta <brendacosta23@gmail.com>
2022-08-26 12:18:15 -0300

    versão 0.1

---
+++ main.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10, 
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+  
+    }
+  });
+
+ 
